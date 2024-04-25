29ba77a5ad35ada09ba2a8f20c92a46b41b892b2
22j5073 <22j5073@stu.meisei-u.ac.jp>
2024-04-25 17:20:30 +0900

    first commit

---
+++ testar-main/testar-main/main.js
@@ -0,0 +1,33 @@
+import { loadGLTF } from "./libs/loader.js";
+const THREE = window.MINDAR.IMAGE.THREE;
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async () => {
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: './assets/targets/cannon.mind'
+    });
+    const { renderer, scene, camera } = mindarThree;
+
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    scene.add(light);
+
+    // const test = await loadGLTF('./assets/models/cannon.gltf');
+    // test.scene.scale.set(0.1, 0.1, 0.1);
+    // test.scene.position.set(0, 0, 0);
+    const test = await loadGLTF('./assets/models/scanned_cannon.gltf');
+    test.scene.scale.set(0.01, 0.01, 0.01);
+    test.scene.position.set(0, 0, 0);
+    // house.scene.rotation.set(0, -0.2, 0);
+
+    const testAncor = mindarThree.addAnchor(0);
+    testAncor.group.add(test.scene);
+
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  }
+  start();
+});
