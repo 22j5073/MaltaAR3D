212faefddb4053fb7a0c44457366894cd5b3c314
22j5073 <22j5073@stu.meisei-u.ac.jp>
2024-04-25 17:52:51 +0900

    all file

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
 
-    // const test = await loadGLTF('./assets/models/cannon.gltf');
+    // const test = await loadGLTF('./assets/models/cannon.glb');
     // test.scene.scale.set(0.1, 0.1, 0.1);
     // test.scene.position.set(0, 0, 0);
-    const test = await loadGLTF('./assets/models/scanned_cannon.gltf');
+    const test = await loadGLTF('./assets/models/scanned_cannon.glb');
     test.scene.scale.set(0.01, 0.01, 0.01);
     test.scene.position.set(0, 0, 0);
     // house.scene.rotation.set(0, -0.2, 0);
